dd0fdc3d27e25154ab8b31344992c73ffc53ecc3
Lucas césar <lcs.cesar1998@gmail.com>
2026-03-14 00:03:03 +0000

    fix: corrigir tipagem do root no index

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const container = document.getElementById("root");
 
-root.render(<App />);
+if (container) {
+  const root = ReactDOM.createRoot(container);
+  root.render(<App />);
+}
