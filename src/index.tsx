874ea32790dbcc68e48b05f19d61c1a201164590
Lucas césar <lcs.cesar1998@gmail.com>
2026-03-14 00:30:33 +0000

    versao real

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
@@ -7,5 +6,5 @@ const container = document.getElementById("root");
 
 if (container) {
   const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  root.render(App());
 }
